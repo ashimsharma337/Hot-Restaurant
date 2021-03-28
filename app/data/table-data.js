e811c11301c4b11fc0c4f13e88ba88890640eab5
ashimsharma337 <you@example.com>
2021-03-28 16:52:10 +0545

    Script file added to the data folder

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+        customerName: 'John',
+        customerEmail: 'john223@example.com',
+        customerID: 'jqui34',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+        customerName: 'sean',
+        customerEmail: 'sean334@example.com',
+        customerID: 'sean43',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+module.exports = waitingArray;
